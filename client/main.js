1b3c71bd287c0811419314c7387cfdc056447dd8
Jaz Singh <jsingh@practichem.com>
2017-01-09 13:52:29 -0600

    Add event listener as a memory leak

--- client/main.js
+++ client/main.js
@@ -3,15 +3,24 @@ import { Template } from 'meteor/templating';
 import './main.html';
 
 var largeString = undefined;
+var count = 0;
 
 Template.Test.events({
 	"click #allocate-test"(event, instance){
 		if(largeString === undefined){
-			largeString = Array(4 * 1024 * 1024 + 1).join("0123456789ABCDEF"); // 64M characters
+			count += 1;
+			largeString = Array(count * 1024 * 1024 + 1).join("0123456789ABCDEF");
 			console.log(largeString.length);
 		} else {
 			console.log("Letting go of largeString");
 			largeString = undefined;
+		}
+
+		if(largeString !== undefined){
+			var someObject = {largeString: largeString};
+			$("#notify-test").on("click", function(){
+				console.log(someObject.largeString.length);
+			});
 		}
 	},
 });

ef107912567f555edb5d3a4b756eb4e7307b055c
Jaz Singh <jsingh@practichem.com>
2017-01-09 13:42:45 -0600

    Add simple memory profiler

--- client/main.js
+++ client/main.js
@@ -1,1 +1,17 @@
+import { Template } from 'meteor/templating';
+
 import './main.html';
+
+var largeString = undefined;
+
+Template.Test.events({
+	"click #allocate-test"(event, instance){
+		if(largeString === undefined){
+			largeString = Array(4 * 1024 * 1024 + 1).join("0123456789ABCDEF"); // 64M characters
+			console.log(largeString.length);
+		} else {
+			console.log("Letting go of largeString");
+			largeString = undefined;
+		}
+	},
+});
